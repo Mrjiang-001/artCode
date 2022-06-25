95832c2a48a114beb2bc9ffbd9637173ed555978
mhqf <2214788954@qq.com>
2022-06-25 18:35:28 +0800

    项目公告包

---
+++ artCode-common/src/test/java/com/artcode/ArtCodeCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtCodeCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
