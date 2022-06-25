39fb4d318bfcad9cc6f4bd12f282031a1c6d51a0
mhqf <2214788954@qq.com>
2022-06-25 18:22:32 +0800

    初始化项目

---
+++ artCode-web/src/main/java/com/artcode/artcodeweb/ArtCodeWebApplication.java
@@ -0,0 +1,13 @@
+package com.artcode.artcodeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtCodeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtCodeWebApplication.class, args);
+    }
+
+}
---
+++ artCode-web/src/test/java/com/artcode/artcodeweb/ArtCodeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artcode.artcodeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtCodeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
